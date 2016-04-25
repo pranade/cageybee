1e8023ed7fb27b9e6875dd7bb777064d4c6f3779
mishaetaya <michaelkaplun@Michaels-MacBook-Air.local>
2016-04-25 07:05:03 -0400

    update to server file

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.get('/', function (req, res) {
   //res.send('Ready for action...');
 });
 
-app.get('/callmemaybe', function (req, res) {
-  console.log("Calling me... maybe");
-  res.sendFile(path.join(__dirname,'/hellomonkey.xml'));
-});
+// app.get('/callmemaybe', function (req, res) {
+//   console.log("Calling me... maybe");
+//   res.sendFile(path.join(__dirname,'/hellomonkey.xml'));
+// });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
