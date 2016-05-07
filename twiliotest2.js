3cc8e929d4fbf47e638a1e67063c4121bbcfaa2a
Pratap Ranade <pratap@Prataps-MacBook-Air.local>
2016-05-07 14:47:52 -0400

    some epic stuff - dynamically retrieving latest user recording

--- twiliotest2.js
+++ twiliotest2.js
@@ -6,10 +6,11 @@ var client = require('twilio')(accountSid, authToken);
 var fs = require('fs');
 var recordingSids = [];
 var recordingURLs = [];
+var recordingWavURLs = [];
+var twilioBaseUrl = "https://api.twilio.com";
 
- 
 client.calls.create({
-    url: 'http://30df94c2.ngrok.io/hellomonkey.xml', // Was localhost:3000, now exposing localhost:3000 to public internet via ngrok
+    url: 'http://db617f8d.ngrok.io/hellomonkey.xml', // Was localhost:3000, now exposing localhost:3000 to public internet via ngrok
     to: '+16503876792', // Log: pranked Matt Insler & Vik
     from: '+16505626104',
     method: 'GET'
@@ -21,33 +22,42 @@ client.calls.create({
 });
 
 client.recordings.list(function(err, data) {
+    var iterator = 0;
     data.recordings.forEach(function(recording) {
+        
         //console.log("Duration: "+recording.Duration);
         //console.log("SID: "+recording.sid);
+        console.log("Iteration: "+iterator);
         recordingSids.push(recording.sid);
-        console.log("URI: https://api.twilio.com/"+recording.uri);
+        //console.log("URI: https://api.twilio.com/"+recording.uri);
         recordingURLs.push(recording.uri);
-    });
+        // List all recordingURLs (where the audio files sit on twilio) and strip off the .json at the end and replace it with .wav
+        //for (var i = 0; i < recordingURLs.length; i++) {
+        // set temp equal to the latest recording URL. Strip off the .json extension and replace it with .wav
+        var temp = recordingURLs[iterator].slice(0,-4);
+        temp = temp.concat("wav");//console.log("Loop element: "+temp);
+        recordingWavURLs.push(temp);
+        iterator++;
+    });    
+    var file = fs.createWriteStream("recording.wav");        
+
+    // OK, need to dump this shit into a callback function... I think I can use client.recordings.list's callback for this
+    var options = {
+      host: 'api.twilio.com',
+      path: recordingWavURLs[0]
+    };
+
+    callback = function(response) {
+      response.pipe(file);
+    }
+
+    https.get(options, callback).end();
 });
 
-// make GET request to retriveve our wav file;
-for (var i = 0; i < recordingURLs.length; i++) {
-	console.log("In for loop");
-	var temp = recordingURLs[i].slice(0,-4);
-	console.log("Loop element: "+temp);
-}
 // Pasted code
-var file = fs.createWriteStream("recording.wav");
-var URL = 'https://api.twilio.com/2010-04-01/Accounts/AC09db92561066aa710c0566b9781d0dbb/Recordings/REda66890b4f47f2842a8980f224ef5b15.wav';
+
+//var URL = 'https://api.twilio.com/2010-04-01/Accounts/AC09db92561066aa710c0566b9781d0dbb/Recordings/REda66890b4f47f2842a8980f224ef5b15.wav';
 
 // //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
-var options = {
-  host: 'api.twilio.com',
-  path: '/2010-04-01/Accounts/AC09db92561066aa710c0566b9781d0dbb/Recordings/REda66890b4f47f2842a8980f224ef5b15.wav'
-};
 
-callback = function(response) {
-  response.pipe(file);
-}
-
-https.get(options, callback).end();
+// Prepare to write file to recording.wav
